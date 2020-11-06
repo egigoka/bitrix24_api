37436bf13e7e546a7d52ae3237af740890908ead
Egor Egorov <egigoka@gmail.com>
2020-11-06 18:20:14 +0500

    init as package

---
+++ bitrix24api/__init__.py
@@ -0,0 +1,72 @@
+from commands import *
+from typing import Union
+from ._version import __version__
+
+
+class BitrixRESTAPI:
+    def __init__(self, link):
+        self.link = link.rstrip("/")
+        self.verify = True
+        print("bitrix_hook", __version__)
+        
+    def disable_ssl_sert_checking(self, bool_: bool):
+        self.verify = not bool_
+    
+    def url_escaping(self, not_escaped_url: str) -> str:
+        output_url = not_escaped_url
+        output_url = output_url.replace("#", "%23")
+        return output_url
+
+    def get(self, method: str, params: dict = imdict()) -> dict:
+        if isinstance(params, dict):
+            params_str = self.dict2str_of_url_params(params)
+        else:
+            raise ValueError("params must be dict")
+
+        url = self.url_escaping(f"{self.link}/{method}?{params_str}")
+        response = Network.get(url, verify=self.verify)
+        Print.colored(response.url, "green")
+        
+        response_json = response.json()
+        try:
+            if response_json["error"] == 'QUERY_LIMIT_EXCEEDED':
+                Time.sleep(1)
+                return self.get(method=method, params=params)
+        except KeyError:
+            pass
+        
+        try:
+            Print.prettify(response.json()['result'])
+            return response.json()['result']
+        except KeyError:
+            Print.prettify(response.json())
+            return response.json()
+
+    def zfill(self, str_like, count_of_zeros):
+        return str(str_like).zfill(count_of_zeros)
+
+    def date2str(self, d):
+        return f"{d.year}-{self.zfill(d.month, 2)}-{self.zfill(d.day, 2)}T{self.zfill(d.hour, 2)}" \
+               f":{self.zfill(d.minute, 2)}:{self.zfill(d.second, 2)}+05:00"
+
+    def dict2list_of_url_params(self, dict_: dict, prefix: str = "", dict_name: Union[str, None] = None) -> list:
+        output_list = []
+        for key, value in dict_.items():
+            if dict_name is None:
+                current_prefix = key
+            else:
+                current_prefix = prefix + f"[{key}]"
+            
+            if isinstance(value, list) or isinstance(value, tuple):
+                value = dict(enumerate(value))
+
+            if isinstance(value, dict):
+                output_list = output_list + self.dict2list_of_url_params(dict_=value, prefix=current_prefix, dict_name=key)
+            else:
+                output_list.append(f"{current_prefix}={value}")
+            # debug Print(f"{prefix=}{current_prefix=}{key=}{value=}{output_list=}")
+        return output_list
+
+    def dict2str_of_url_params(self, dict_: dict) -> str:
+        list_ = self.dict2list_of_url_params(dict_)
+        return "&".join(list_)
---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+__version__ = "undefined"  # if setup fails read it from _version.py and to please PyCharm
+exec(open('bitrix24api/_version.py').read())
+
+setuptools.setup(
+    name='proxy_checker',
+    version=__version__,
+    packages=['bitrix24api'],
+    install_requires=['commands'],
+    author='egigoka',
+    description='Bitrix24 small and dirty api wrapper',
+    long_description="Bitrix24 small and dirty api wrapper"
+)
