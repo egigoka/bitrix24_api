f60a9d5ec9926ad63da169fc444421510bd0cff1
Egor Egorov <egigoka@gmail.com>
2020-11-06 20:40:20 +0500

    added BitrixRESTAPI.smart_get to support multiple page load

--- bitrix24api/__init__.py
+++ bitrix24api/__init__.py
@@ -17,15 +17,16 @@ class BitrixRESTAPI:
         output_url = output_url.replace("#", "%23")
         return output_url
 
-    def get(self, method: str, params: dict = imdict()) -> dict:
+    def get(self, method: str, params: dict = imdict(), verbose=False) -> dict:
         if isinstance(params, dict):
             params_str = self.dict2str_of_url_params(params)
         else:
-            raise ValueError("params must be dict")
+            raise TypeError("params must be dict")
 
         url = self.url_escaping(f"{self.link}/{method}?{params_str}")
         response = Network.get(url, verify=self.verify)
-        Print.colored(response.url, "green")
+        if verbose:
+            Print.colored(response.url, "green")
         
         response_json = response.json()
         try:
@@ -34,15 +35,51 @@ class BitrixRESTAPI:
                 return self.get(method=method, params=params)
         except KeyError:
             pass
-        
-        try:
-            Print.prettify(response.json()['result'])
-            return response.json()['result']
-        except KeyError:
-            Print.prettify(response.json())
-            return response.json()
 
-    def zfill(self, str_like, count_of_zeros):
+        return response_json
+
+    def smart_get(self, method: str,
+                  params: dict = None,
+                  verbose=False) -> dict:
+        output = None
+        if params is None:
+            params = {}
+        while True:
+            params["start"] = 0 if output is None else len(output)
+            response = self.get(method=method, params=params, verbose=verbose)
+
+            # check if there is result
+            try:
+                if verbose:
+                    Print.prettify(response['result'])
+                output_part = response['result']
+            except KeyError:
+                Print.prettify(response)
+                raise
+
+            # if it's first page
+            if output is None:
+                output = output_part
+
+            # else if it's second page
+            elif isinstance(output, dict):
+                output.update(output_part)
+            else:
+                output += output_part
+
+            # if all is loaded
+            try:
+                if verbose:
+                    Print.colored(f"{len(output)} of {response['total']} is loaded", "magenta")
+                if response["total"] <= len(output):
+                    break
+            except KeyError:  # if there's no paged output
+                break
+
+        return output
+
+    @staticmethod
+    def zfill(str_like, count_of_zeros):
         return str(str_like).zfill(count_of_zeros)
 
     def date2str(self, d):
